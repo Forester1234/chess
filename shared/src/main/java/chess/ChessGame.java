bd565d136b68a8a719ad7438ca5b749452d921ec
Forester1234 <jfc82@byu.edu>
2025-10-03 10:58:46 -0600

    impl(p1) fixed uneccesary error finder in validMoves

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -74,7 +74,7 @@ public class ChessGame {
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
         ChessPiece piece = board.getPiece(startPosition);
 
-        if (piece == null || piece.getTeamColor() != currentTurn) return null;
+        if (piece == null) return null;
 
         Collection<ChessMove> possibleMoves = piece.pieceMoves(board, startPosition);
         Collection<ChessMove> legalMoves = new ArrayList<>();
