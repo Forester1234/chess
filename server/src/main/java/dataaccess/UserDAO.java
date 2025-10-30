6a11510388bb7014f584ddac196dc63d58aa4d0c
Forester1234 <jfc82@byu.edu>
2025-10-30 16:53:04 -0600

    impl(p4) made UserDAO utilize MySQL

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -2,21 +2,54 @@ package dataaccess;
 
 import model.UserData;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserDAO {
-    private final Map<String, UserData> users = new HashMap<>();
 
-    public void createUser(UserData user) {
-        users.put(user.username(), user);
+    public void createUser(UserData user) throws DataAccessException {
+        String sql = "INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
+        try (Connection conect = DatabaseManager.getConnection();
+             PreparedStatement stat = conect.prepareStatement(sql)) {
+            stat.setString(1, user.username());
+            stat.setString(2, user.password());
+            stat.setString(3, user.email());
+            stat.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Couldn't create userdata", e);
+        }
     }
 
-    public UserData getUser(String username){
-        return users.get(username);
+    public UserData getUser(String username) throws DataAccessException {
+        String sql = "SELECT username, password, email FROM user WHERE username = ?";
+        try (Connection conect = DatabaseManager.getConnection();
+             PreparedStatement stat = conect.prepareStatement(sql)) {
+            stat.setString(1, username);
+            try (ResultSet resSet = stat.executeQuery()) {
+                if (resSet.next()) {
+                    return new UserData(
+                            resSet.getString("username"),
+                            resSet.getString("password"),
+                            resSet.getString("email")
+                    );
+                } else {
+                    return null;
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Failed to get userData", e);
+        }
     }
 
-    public void clear() {
-        users.clear();
+    public void clear() throws DataAccessException {
+        String sql = "DELETE FROM user";
+        try (Connection conect = DatabaseManager.getConnection();
+             PreparedStatement stat = conect.prepareStatement(sql)) {
+            stat.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Failed to clear users", e);
+        }
     }
 }
