805be9f76dd8800dafde82614c44e95095b9f0c2
Forester1234 <jfc82@byu.edu>
2025-11-14 13:03:18 -0700

    impl(p5) began setup for ServerFacadeTests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,21 +1,45 @@
 package client;
 
 import facade.ServerFacade;
+import exception.ResponseException;
+
 import org.junit.jupiter.api.*;
 import server.Server;
+
+import service.registerr.RegisterRequest;
+import service.registerr.RegisterResult;
+import service.loginr.LoginRequest;
+import service.loginr.LoginResult;
+import service.joinr.JoinRequest;
+import service.joinr.JoinResult;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
     static ServerFacade facade;
+    private static int port;
 
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
         facade = new ServerFacade(port);
+    }
+
+    @BeforeEach
+    void clearDB() throws Exception {
+        var client = java.net.http.HttpClient.newHttpClient();
+        var request = java.net.http.HttpRequest.newBuilder()
+                .uri(java.net.URI.create("http://localhost:" + port + "/db"))
+                .method("DELETE", HttpRequest.BodyPublishers.noBody())
+                .build();
+
+        client.send(request, HttpResponse.BodyHandlers.ofString());
     }
 
     @AfterAll
